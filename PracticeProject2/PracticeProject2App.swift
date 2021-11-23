66f76b16371980b60faec91c79f932a0bf67fb76
Luka <vujnovacluka89@gmail.com>
2021-11-23 21:43:47 +0100

    Initial Commit

---
+++ PracticeProject2/PracticeProject2App.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeProject2App.swift
+//  PracticeProject2
+//
+//  Created by Luka Vujnovac on 23.11.2021..
+//
+
+import SwiftUI
+
+@main
+struct PracticeProject2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
