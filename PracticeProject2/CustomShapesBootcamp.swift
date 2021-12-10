5f4e124c76653e8222fdec4e00370e1a6e9012b2
Luka <vujnovacluka89@gmail.com>
2021-12-10 19:39:56 +0100

    CustomShape bootcamp completed

--- PracticeProject2/CustomShapesBootcamp.swift
+++ PracticeProject2/CustomShapesBootcamp.swift
@@ -7,9 +7,56 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        Path {path in 
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        }
+    }
+}
+
+struct Diamond: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path {path in 
+            let horizontalOffset: CGFloat = rect.width * 0.2
+            
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - horizontalOffset, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        }
+    }
+}
+
+struct Trapezoid: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path{path in 
+            let horizontalOffset: CGFloat = rect.width * 0.2
+
+            path.move(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - horizontalOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.minY))
+        }
+    }
+}
+
 struct CustomShapesBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+//            Triangle()
+//                .frame(width: 300, height: 300)
+            
+//            Diamond()
+            Trapezoid()
+                .frame(width: 300, height: 100)
+        }
     }
 }
 
